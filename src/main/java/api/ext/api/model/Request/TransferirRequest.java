ecb42f057c1092b8bbf4f770843cd78e3f59dfca
victorribeirog <vicrgon2010@hotmail.com>
2024-10-01 16:33:57 -0300

    Adicionando funcionalidade Pagamento Pix por Chave

--- src/main/java/api/ext/api/model/Request/TransferirRequest.java
+++ src/main/java/api/ext/api/model/Request/TransferirRequest.java
@@ -2,6 +2,7 @@ package api.ext.api.model.Request;
 
 import api.ext.api.model.Enum.PrioridadePix;
 import api.ext.api.model.Enum.TipoQRCode;
+import api.ext.api.model.ContaDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class TransferirRequest {
     String codigoOperacaoCliente;
     String descricaoCliente;
     PrioridadePix prioridadePix;
+    ContaDto contaOrigem;
     String informacoesAdicionais;
     String idConciliacaoRecebedor;
     TipoQRCode tipoQRCode;
