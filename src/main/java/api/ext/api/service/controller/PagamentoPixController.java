12c36524a88255b33f55a0ee7d096c0b1f0f4176
victorribeirog <vicrgon2010@hotmail.com>
2024-10-01 16:06:49 -0300

    adicionando a funcionalidade de pagamento via Chave Pix

--- src/main/java/api/ext/api/service/controller/PagamentoPixController.java
+++ src/main/java/api/ext/api/service/controller/PagamentoPixController.java
@@ -27,23 +27,15 @@ public class PagamentoPixController {
             @RequestHeader(value = "IgnoraHandshake", defaultValue = "true") boolean ignoraHandshake,
             @RequestBody TransferirRequest transferirRequest) {
 
-        // Verificar se o IdempotencyKey já foi utilizado
         Optional<TransferirResponse> existingResponse = pagarPixChaveService.findByIdempotencyKey(idempotencyKey);
         if (existingResponse.isPresent()) {
-            // Retorna a resposta armazenada previamente para evitar duplicações
+
             TransferirResponse response = existingResponse.get();
             return ResponseEntity.status(HttpStatus.OK).body(response);
         }
 
-        // Se IgnoraHandshake for falso, você pode realizar qualquer lógica adicional antes de processar a transação
-        if (!ignoraHandshake) {
-            // Lógica de handshake aqui (por exemplo, validações adicionais)
-        }
 
-        // Processar a transação normalmente
         TransferirResponse response = pagarPixChaveService.transferir(transferirRequest);
-
-        // Armazenar a resposta para lidar com idempotência
         pagarPixChaveService.saveTransaction(idempotencyKey, response);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
